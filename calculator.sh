f572399cf82e5e7b86c0056bd416330df7b6970d
Surafelll <surafelwondimagegn01@gmail.com>
2022-05-31 09:56:24 -0700

    created calculator in shell

---
+++ calculator.sh
@@ -0,0 +1,17 @@
+clear
+echo "a shell script to calculat +,-,/,*,%"
+echo "Enter number1"
+read number1
+echo "Enter number2"
+read number2
+sum=$(($number1+$number2))
+echo "sum is : $sum "
+sub=$(($number1-$number2))
+echo "subtraction is : $sub "
+mult=$(($number1*$number2))
+echo "Multiplication is : $mult "
+div=$(($number1/$number2))
+echo "Division is : $div "
+rem=$(($number1%$number2))
+echo "remainder is : $rem "
+
