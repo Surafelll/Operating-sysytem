7ab3403bf7cae6dde213614845ab1f8ca615cf3b
Surafelll <surafelwondimagegn01@gmail.com>
2022-06-01 09:47:18 -0700

    find  greater two number is edited comment read numbers

--- week one/greater2number.sh
+++ week one/greater2number.sh
@@ -1,9 +1,9 @@
 clear
 echo "a shell script to find out the large between two number." //details about all program
 echo "First number1 :" //message to put in first number 
-read number1 //take number one and read it
-echo "Second number2:" //take number one and read it
-read number2
+read number1 //take number one from user and read it
+echo "Second number2:" //message to put in first number 
+read number2 //take number two from user and read it
 
 if test $number1 -gt $number2 
 then
