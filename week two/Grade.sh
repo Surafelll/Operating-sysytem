e81a00cfa27d2f714989749b0fae5baea0170079
Surafelll <surafelwondimagegn01@gmail.com>
2022-06-03 22:31:43 -0700

    comment

--- week two/Grade.sh
+++ week two/Grade.sh
@@ -3,7 +3,7 @@ echo "STUDENT GRADE OF 3 SUBJECT SHELL SCRIPT"
 echo "Enter marks(out of 100) of "
 read -p "Subject 1: " s1 //start a process at a specific priority
 read -p "Subject 2: " s2 //start a process at a specific priority
-read -p "Subject 3: " s3 //start a process at a specific priority
+read -p "Subject 3: " s3 //start a process at a specific priority.
 sum=$(( $s1 + $s2 + $s3))
 echo "Sum of marks of 3 subjects is : "$sum
 per=`expr $sum / 3`
